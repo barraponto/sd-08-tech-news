1927c792d8f3019d0545008fbae02a60ed0fd49f
Gabriel-Goncalves <gabriel_98gm@hotmail.com>
2021-09-02 19:15:20 -0300

    adicionando requisito 7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,8 +1,10 @@
+from datetime import datetime
 from tech_news.database import search_news
 
 
 # Requisito 6
 def search_by_title(title):
+    # source: https://docs.mongodb.com/manual/reference/operator/query/regex/
     mongo_query = {"title": {"$regex": title, "$options": "i"}}
     news = search_news(mongo_query)
     tuple_news = [
@@ -11,9 +13,25 @@ def search_by_title(title):
     return tuple_news
 
 
+def is_a_valid_data(date):
+    try:
+        datetime.strptime(date, "%Y-%m-%d")
+        return True
+    except ValueError:
+        return False
+
+
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    if is_a_valid_data(date) is False:
+        raise ValueError("Data inválida")
+    else:
+        mongo_query = {"timestamp": {"$regex": date, "$options": "i"}}
+        news = search_news(mongo_query)
+        tuple_news = [
+            (actual_news["title"], actual_news["url"]) for actual_news in news
+        ]
+        return tuple_news
 
 
 # Requisito 8
