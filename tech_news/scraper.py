b1b7576f3a42971065cd4ca406ae1f4a3d9b7467
Lucas Rodrigues <lucas.movimento@gmail.com>
2021-09-02 18:47:36 -0300

    Implementa o metodo scrape_noticia para serializar as informaçoes de determinada noticia em um dicionario.

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,5 +1,6 @@
 import requests
 import time
+from parsel import Selector
 
 
 # Requisito 1
@@ -12,6 +13,7 @@ def fetch(url):
         response = requests.get(url, timeout=3)
         if response.status_code != 200:
             return None
+        # print(response.text)
         return response.text
     except requests.exceptions.Timeout:
         return None
@@ -19,7 +21,42 @@ def fetch(url):
 
 # Requisito 2
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    """Returns a dict with the selected info from a html content"""
+    selector = Selector(text=html_content)
+    return {
+        "url": selector.css('link[rel="canonical"]::attr(href)').get(),
+        "title": selector.css("h1#js-article-title::text").get(),
+        "timestamp": (
+            selector.css(".tec--timestamp__item")
+            .xpath("./time/@datetime")
+            .get()
+        ),
+        "writer": selector.css(".tec--author__info__link::text").get().strip(),
+        "shares_count": int(
+            selector.css(".tec--toolbar__item::text")
+            .get()
+            .strip()
+            .split(" ")[0]
+        ),
+        "comments_count": int(
+            selector.css("button#js-comments-btn::attr(data-count)").get()
+        ),
+        "summary": selector.css(".tec--article__body")
+        .xpath("string(./p)")
+        .get(),
+        "sources": [
+            source.strip()
+            for source in selector.xpath('//h2[text() = "Fontes"]/..')
+            .css(".tec--badge ::text")
+            .getall()
+        ],
+        "categories": [
+            category.strip()
+            for category in selector.css(
+                "div#js-categories > a::text"
+            ).getall()
+        ],
+    }
 
 
 # Requisito 3
