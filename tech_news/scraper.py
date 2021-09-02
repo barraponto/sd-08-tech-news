a37c4c311c04088f30c8feb6df9e3d39eb9aeb44
victor Farias <victorfarias.tv@gmail.com>
2021-09-02 16:39:35 -0300

    Requisito 1

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,7 +1,19 @@
+import time
+import requests
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
-    """First Commit"""
+    time.sleep(1)
+    try:
+        response = requests.get(url, timeout=3)
+    except requests.ReadTimeout:
+        return None
+
+    if response.status_code != 200:
+        return None
+
+    return response.text
 
 
 # Requisito 2
