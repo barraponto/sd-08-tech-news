41c147780fec83f34320a144f21301c390d8d767
hugobrr <hugobrr@gmail.com>
2021-09-02 20:39:59 -0300

    Segundo requisito

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,6 @@
 import requests
 from time import sleep
-
+from parsel import Selector
 from requests.exceptions import Timeout
 
 
@@ -16,12 +16,67 @@ def fetch(url):
     if response.status_code != 200:
         return None
     return response.text
-    """Seu código deve vir aqui"""
 
 
 # Requisito 2
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    url = selector.css("link").xpath("./@href").extract()[-2]
+    title = (
+        selector.css(".tec--article__header__title").xpath("//h1/text()").get()
+    )
+    timestamp = selector.css("time").xpath("@datetime").get()
+    writer = (
+        selector.css(".tec--author__info__link").xpath("text()").get().strip()
+        or None
+    )
+    # || None
+    shares_count = (
+        int(
+            selector.css(".tec--toolbar__item")
+            .xpath("text()")
+            .get()
+            .split()[0]
+        )
+        or 0
+    )
+    # || 0
+    comments_count = (
+        int(
+            selector.css(".tec--toolbar__item")[1]
+            .xpath("//button/text()")
+            .getall()[1]
+            .split()[0]
+        )
+        or 0
+    )
+    summary = "".join(selector.xpath("//p")[2].css("*::text").getall())
+    len_badge = len(selector.css(".tec--badge").xpath("text()").getall())
+    len_primary = len(
+        selector.css(".tec--badge--primary").xpath("text()").getall()
+    )
+    sources_not_striped = (
+        selector.css(".tec--badge")
+        .xpath("text()")
+        .getall()[: (len_badge - len_primary)]
+    )
+    sources = [item.strip() for item in sources_not_striped]
+    categories_not_striped = (
+        selector.css(".tec--badge--primary").xpath("text()").getall()
+    )
+    categories = [item.strip() for item in categories_not_striped]
+    result = {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "shares_count": shares_count,
+        "comments_count": comments_count,
+        "summary": summary,
+        "sources": sources,
+        "categories": categories,
+    }
+    return result
 
 
 # Requisito 3
