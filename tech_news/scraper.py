fd7d49a022af684a9961bfb57999b8e5aad75514
Gabriel-Goncalves <gabriel_98gm@hotmail.com>
2021-09-02 16:50:46 -0300

    adicionando requisito 4

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -79,7 +79,9 @@ def scrape_novidades(html_content):
 
 # Requisito 4
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    link_next_page = selector.css(".tec--btn::attr(href)").get()
+    return link_next_page
 
 
 # Requisito 5
