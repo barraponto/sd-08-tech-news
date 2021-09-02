8efdc3e2343524eec0eca432012bae2246b1b459
Leandro Lachini <leandrolachini@gmail.com>
2021-09-02 15:17:00 -0300

    task 1

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,22 @@
+import requests
+import time
+# from parsel import Selector
+
+
 # Requisito 1
 def fetch(url):
     """Seu código deve vir aqui"""
+    try:
+        response = requests.get(url, timeout=3)
+        time.sleep(1)
+        print(response.text)
+        print(response.status_code)
+        if response.status_code == 200:
+            return response.text
+        else:
+            return None
+    except requests.ReadTimeout:
+        return None
 
 
 # Requisito 2
