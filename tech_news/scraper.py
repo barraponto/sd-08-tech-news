cd35b0b1e05c870d4008750cb4bce1e27b37a514
Gabriel-Goncalves <gabriel_98gm@hotmail.com>
2021-09-02 16:09:54 -0300

    adicionando requisitos 2

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,5 +1,5 @@
 import requests
-# from parsel import Selector
+from parsel import Selector
 import time
 
 
@@ -13,7 +13,7 @@ def fetch(url):
         requests.exceptions.HTTPError,
         requests.exceptions.TooManyRedirects,
         requests.exceptions.Timeout,
-        requests.exceptions.ConnectionError
+        requests.exceptions.ConnectionError,
     ):
         return None
 
@@ -25,7 +25,48 @@ def fetch(url):
 
 # Requisito 2
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    news = {}
+    selector = Selector(text=html_content)
+
+    url = selector.css("link[rel='canonical']::attr(href)").get()
+    news["url"] = url
+
+    title = selector.css(".tec--article__header__title::text").get()
+    news["title"] = title
+
+    timestamp = selector.css("#js-article-date::attr(datetime)").get()
+    news["timestamp"] = timestamp
+
+    writer = selector.css(".tec--author__info__link::text").get()
+    news["writer"] = writer.strip()
+
+    shares_count = selector.css(".tec--toolbar__item::text").getall()
+    shares_count = int(shares_count[0].split()[0])
+    news["shares_count"] = shares_count
+
+    comments_count = selector.css("#js-comments-btn::attr(data-count)").get()
+    if comments_count is None:
+        news["comments_count"] = 0
+    else:
+        news["comments_count"] = int(comments_count)
+
+    summary = selector.css(
+        ".tec--article__body > p:first-child *::text"
+    ).getall()
+    # source: https://pt.stackoverflow.com/questions/324979/como-concatenar
+    # -itens-de-uma-lista-em-python
+    summary = "".join(summary)
+    news["summary"] = summary
+
+    sources = selector.css(".z--mb-16 a.tec--badge::text").getall()
+    sources = [source.strip() for source in sources]
+    news["sources"] = sources
+
+    categories = selector.css("#js-categories a.tec--badge::text").getall()
+    categories = [category.strip() for category in categories]
+    news["categories"] = categories
+
+    return news
 
 
 # Requisito 3
