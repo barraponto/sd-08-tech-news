ae14389c8333b79db24ee5585cde5238fb78d61f
Edson Araujo de Barros Junior <edson@pop-os.localdomain>
2021-09-02 15:07:56 -0300

    Req 1

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,19 @@
+import requests
+# from parsel import Selector
+import time
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    time.sleep(1)
+    try:
+        response = requests.get(url, timeout=3)
+        if response.status_code == 200:
+            return response.text
+        else:
+            return None
+    except requests.Timeout:
+        return None
 
 
 # Requisito 2
