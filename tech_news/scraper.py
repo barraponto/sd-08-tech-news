34d94c14face3b8f7fef8157ae1ea96b1dbe9c1f
Rodrigo M Sudario <monteiro.sudario@gmail.com>
2021-09-02 10:49:08 -0300

    Requisito 2 e mudança no arquivo dev-requirements

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,5 +1,6 @@
 import requests
 import time
+from parsel import Selector
 
 
 # Requisito 1
@@ -19,6 +20,28 @@ def fetch(url):
 # Requisito 2
 def scrape_noticia(html_content):
     """Seu código deve vir aqui"""
+    # https://css-tricks.com/almanac/selectors/f/first-child/
+    selector = Selector(html_content)
+    shares_count = selector.css(".tec--toolbar__item::text").re_first(r"\d+")
+    comments_count = selector.css("#js-comments-btn::text").re_first(r"\d+")
+    writer = selector.css(".tec--author__info__link::text").get()
+    summary = selector.css(
+        ".tec--article__body > p:first-child *::text"
+    ).getall()
+    sources = selector.css("div.z--mb-16 .tec--badge::text").getall()
+    categories = selector.css("div#js-categories a.tec--badge::text").getall()
+    newsInfo = {
+        "url": selector.css("meta[property='og:url']::attr(content)").get(),
+        "title": selector.css(".tec--article__header__title::text").get(),
+        "timestamp": selector.css("#js-article-date::attr(datetime)").get(),
+        "writer": writer.strip() if writer else writer,
+        "shares_count": int(shares_count) if shares_count else 0,
+        "comments_count": int(comments_count) if comments_count else 0,
+        "summary": "".join(summary),
+        "sources": list(map(str.strip, sources)),
+        "categories": list(map(str.strip, categories))
+    }
+    return newsInfo
 
 
 # Requisito 3
