47982448b8daf727af71af1ff4156634c500353a
Rodrigo M Sudario <monteiro.sudario@gmail.com>
2021-09-03 10:24:26 -0300

    Iniciando o Requisito 10: modificando o arquivo ratings.py

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,22 @@
+from tech_news.database import find_news
+from operator import itemgetter
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news = []
+    top_five = []
+    search = find_news()
+    for unity_new in search:
+        popularity = unity_new["shares_count"] + unity_new["comments_count"]
+        title_popularity = (unity_new["title"], popularity)
+        news.append(title_popularity)
+
+    sorted(news, key=itemgetter(2), reverse=True)
+    for index in range(5):
+        title_url = (news["title"][index], news["url"][index])
+        top_five.append(title_url)
+    return news
 
 
 # Requisito 11
