95f9e83691b5a2f0c75128410259cd03b130592d
Leandro Lachini <leandrolachini@gmail.com>
2021-09-03 14:54:06 -0300

    task 2

--- tech_news/tempCodeRunnerFile.py
+++ tech_news/tempCodeRunnerFile.py
@@ -86,23 +86,8 @@ def scrape_noticia(html_content):
     categories = [categorie.strip() for categorie in categories_selector]
     # print(categories_selector)
     news_dict['categories'] = categories
-    # print(news_dict)
+    print(news_dict)
     return news_dict
 
 
-# scrape_noticia(fetch(news_page))
-
-
-# Requisito 3
-def scrape_novidades(html_content):
-    """Seu código deve vir aqui"""
-
-
-# Requisito 4
-def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
-
-
-# Requisito 5
-def get_tech_news(amount):
-    """Seu código deve vir aqui"""
+scrape_noticia(fetch(news_page))
