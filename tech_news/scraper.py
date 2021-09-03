6403a01131cb451fafbd893d418010b5963a7e48
Thiago Ferreira <thiagocristhian@gmail.com>
2021-09-02 23:42:36 -0300

    Requisito 2

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,8 @@
 import requests
 import time
 
+from parsel import Selector
+
 
 # Requisito 1
 def fetch(url):
@@ -19,6 +21,46 @@ def fetch(url):
 # Requisito 2
 def scrape_noticia(html_content):
     """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    str_url = "head link[rel=canonical]::attr(href)"
+    url = selector.css(str_url).get()
+    str_title = "#js-article-title::text"
+    title = selector.css(str_title).get()
+    str_timestamp = "#js-article-date::attr(datetime)"
+    timestamp = selector.css(str_timestamp).get()
+    str_writer = "p.z--font-bold a::text"
+    writer = selector.css(str_writer).get()
+    str_shares = "nav.tec--toolbar button.tec--btn"
+    shares_count = selector.css(str_shares).re_first(r'\d+')
+    str_comments = "#js-comments-btn::attr(data-count)"
+    comments_count = selector.css(str_comments).get()
+    str_summary = "div.tec--article__body p:first-child *::text"
+    summary = selector.css(str_summary).getall()
+    summary_formated = []
+    for element in summary:
+        summary_formated.append(element.strip())
+    str_sources = "div div.z--mb-16 div a::text"
+    sources = selector.css(str_sources).getall()
+    sources_formated = []
+    for element in sources:
+        sources_formated.append(element.strip())
+    str_categories = "div div a.tec--badge.tec--badge--primary::text"
+    categories = selector.css(str_categories).getall()
+    categories_formated = []
+    for element in categories:
+        categories_formated.append(element.strip())
+
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer.strip(),
+        "shares_count": int(shares_count),
+        "comments_count": int(comments_count),
+        "summary": ' '.join(summary_formated),
+        "sources": sources_formated,
+        "categories": categories_formated,
+    }
 
 
 # Requisito 3
