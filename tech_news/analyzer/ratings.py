a64e6ec4814cd8dd5ecb160dd747485b79e1782a
hugobrr <hugobrr@gmail.com>
2021-09-03 15:57:59 -0300

    Requisito 11

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -27,6 +27,32 @@ def top_5_news():
     """Seu código deve vir aqui"""
 
 
+def get_categories_values(categories):
+    return categories[1]
+
+
+def get_categories_name(categories):
+    return categories[0]
+
+
 # Requisito 11
 def top_5_categories():
+    search_list = find_news()
+    categories = {}
+    for item in search_list:
+        for category in item["categories"]:
+            try:
+                categories[category] = 1
+            except KeyError:
+                categories.update({category: 1})
+            else:
+                categories[category] += 1
+    list_news = list(categories.items())
+    list_news.sort(key=get_categories_name)
+    list_news.sort(key=get_categories_values, reverse=True)
+    ordered_list = []
+    for item in list_news[:5]:
+        ordered_list.append(item[0])
+    print(ordered_list)
+    return ordered_list
     """Seu código deve vir aqui"""
