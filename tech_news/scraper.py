77b9c6d91bb2a5ecda462fd7f001dbef33007a49
Bruno Martins <bruno.martins217@gmail.com>
2021-09-02 22:49:15 -0300

    Requisito 2

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,8 +1,9 @@
 import requests
 import time
+from parsel import Selector
+
+
 # Requisito 1
-
-
 def fetch(url):
     try:
         response = requests.get(url)
@@ -16,7 +17,56 @@ def fetch(url):
 
 # Requisito 2
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+
+    def shares_count():
+        if int(selector.css(".tec--toolbar__item::text")
+                .get().split(" ")[1] != 0):
+            return (
+                int(
+                    selector.
+                    css(".tec--toolbar__item::text").get().split(" ")[1]))
+        return 0
+
+    def comments_count():
+        if int(selector.css("#js-comments-btn::attr(data-count)").get() != 0):
+            return (
+                int(selector.css("#js-comments-btn::attr(data-count)").get()))
+        return 0
+
+    return({
+            "url": selector.css("head link[rel='canonical']::attr(href)")
+            .get(),
+
+            "title": selector.css("head title::text").get().split("-")[0]
+            .strip(),
+
+            "timestamp": selector.css("#js-article-date::attr(datetime)")
+            .get(),
+
+            "writer": selector.css(".tec--author__info__link::text").get()
+            .strip(),
+
+            "shares_count": shares_count(),
+
+            "comments_count": comments_count(),
+
+            "summary": "".join(selector.css(
+                ".tec--article__body p:first-child ::text"
+            ).getall()),
+            # https://www.simplilearn.com/tutorials/python-tutorial/list-to-string-in-python
+
+            "sources": list(map(
+                str.strip,
+                (selector.css("div.z--mb-16 .tec--badge::text").getall()))),
+
+            "categories": list(map(str.strip, selector.css(
+                    "#js-categories a.tec--badge::text"
+                ).getall())),
+        })
+
+
+scrape_noticia(fetch("https://www.tecmundo.com.br/mobilidade-urbana-smart-cities/155000-musk-tesla-carros-totalmente-autonomos.htm"))
 
 
 # Requisito 3
