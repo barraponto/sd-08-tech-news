464f349987553ac9a00c88d412e90b20e2797e12
Seu nome <luiza.dilly@gmail.com>
2021-09-03 10:39:05 -0300

    req 1

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,8 +1,18 @@
+import requests
+import time
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
-
-
+    try:
+        response = requests.get(url, timeout=3)
+        time.sleep(1)
+        if response.status_code != 200:
+            return None
+        else:
+            return response.text
+    except requests.ReadTimeout:
+        return None
+        
 # Requisito 2
 def scrape_noticia(html_content):
     """Seu código deve vir aqui"""
