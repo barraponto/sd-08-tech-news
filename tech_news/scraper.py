648d2ecd1ae681c712e8370086c8aaf95ccbb727
albertassi88 <albertassi88@gmail.com>
2021-09-03 17:38:15 -0300

    Requisito 3

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -52,7 +52,10 @@ def scrape_noticia(html_content):
 
 # Requisito 3
 def scrape_novidades(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    return selector.css(
+        ".tec--list__item  .tec--card__title__link::attr(href)"
+    ).getall()
 
 
 # Requisito 4
