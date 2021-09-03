e77594b11808c46f0ec7685382421e878983ff15
Seu nome <luiza.dilly@gmail.com>
2021-09-03 17:14:05 -0300

    req 2 in progress

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,5 +1,7 @@
 import requests
 import time
+from parsel import Selector
+
 
 # Requisito 1
 def fetch(url):
@@ -12,10 +14,25 @@ def fetch(url):
             return response.text
     except requests.ReadTimeout:
         return None
-        
+
+
 # Requisito 2
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    # https://rockcontent.com/br/blog/canonical-tag/#oque
+    url = selector.css("link[rel=canonical] ::attr(href)").get()
+    title = selector.css(
+        "#js-article-title ::text"
+    ).get()
+    time = selector.css("time ::attr(datetime)").get()
+    writer = selector.css(".tec--author__info__link ::text").get()
+    time = selector.css("time ::attr(datetime)").get()
+    shares_count = selector.css(".tec--toolbar__item ::text").get()
+    comments_count = selector.css("#js-comments-btn ::attr(data-count)").get()
+    summary = selector.css(
+        "div.tec--article__body z--px-16 p402_premium > p:nth-child(1) *::text"
+    ).getall()
+    print(url, title, writer, time, shares_count, comments_count, summary)
 
 
 # Requisito 3
