86410be8ea2f8b154ab72df4eca7bd0d40467dc8
Bruno Martins <bruno.martins217@gmail.com>
2021-09-03 09:31:22 -0300

    Requisito 3

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -68,7 +68,11 @@ def scrape_noticia(html_content):
 
 # Requisito 3
 def scrape_novidades(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    return selector.css("h3 .tec--card__title__link::attr(href)").getall()
+
+
+# scrape_novidades(fetch("https://www.tecmundo.com.br/novidades"))
 
 
 # Requisito 4
