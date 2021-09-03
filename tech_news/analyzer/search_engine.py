d0ca987bceba9c4681ca94844736fe7a16191e97
Bruno Wesley <bruno.wesley77@gmail.com>
2021-09-03 17:01:24 -0300

    req 8

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -27,6 +27,11 @@ def search_by_date(date):
 # Requisito 8
 def search_by_source(source):
     """Seu código deve vir aqui"""
+    query = {"sources": {"$regex": f".*{source}*.", "$options": "i"}}
+    news_list = search_news(query)
+    tuples_list = [(news["title"], news["url"]) for news in news_list]
+
+    return tuples_list
 
 
 # Requisito 9
