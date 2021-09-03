5846fd312d9d255ed33b271a9b31b78c8f743faf
victor Farias <victorfarias.tv@gmail.com>
2021-09-03 13:16:30 -0300

    Requisito 2

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -19,7 +19,56 @@ def fetch(url):
 
 # Requisito 2
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    url = selector.css(
+        "link[rel=canonical]::attr(href)"
+    ).get()
+    title = selector.css(
+        "h1.tec--article__header__title::text"
+    ).get()
+    timestamp = selector.css(
+        "time::attr(datetime)"
+    ).get()
+    writer = selector.css(
+        ".tec--author__info__link::text"
+    ).get().strip()
+    shares_count = selector.css(
+        "div.tec--toolbar__item::text"
+    ).get()
+    shares_count = int(shares_count.split()[0]) or 0
+    comments_count = int(
+        selector.css(
+            "#js-comments-btn::attr(data-count)"
+        ).get()
+    )
+    summary = "".join(
+        selector.css(
+            "div.tec--article__body > p:nth-child(1) *::text"
+        ).getall()
+    )
+    sources = [
+        source.strip()
+        for source in selector.xpath(
+            "//h2[text()='Fontes']/following-sibling::div//a/text()"
+        ).getall()
+    ]
+    categories = [
+        category.strip()
+        for category in selector.xpath(
+            "//h2[text()='Categorias']/following-sibling::div//a/text()"
+        ).getall()
+    ]
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "shares_count": shares_count,
+        "comments_count": comments_count,
+        "summary": summary,
+        "sources": sources,
+        "categories": categories,
+    }
 
 
 # Requisito 3
