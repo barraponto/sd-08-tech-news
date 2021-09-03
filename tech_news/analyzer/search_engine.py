78ee8bed55d072738f7b6b7121625a4462e085c7
Lucas Rodrigues <lucas.movimento@gmail.com>
2021-09-03 17:18:24 -0300

    Implementa metodo search_by_title para buscar no DB noticias pelos titulos.

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,16 @@
+import re
+from tech_news.database import get_collection, search_news
+
 # Requisito 6
 def search_by_title(title):
     """Seu código deve vir aqui"""
+    news_list = search_news({"title": re.compile(title, re.IGNORECASE)})
+    if not news_list:
+        return []
+    result = []
+    for news in news_list:
+        result.append((news["title"], news["url"]))
+    return result
 
 
 # Requisito 7
