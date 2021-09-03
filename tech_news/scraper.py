a9f1594ea48bbb2a4418a7e9d477b03b2cb07609
Lucas Rodrigues <lucas.movimento@gmail.com>
2021-09-03 16:42:25 -0300

    Implementa metodo get_tech_news para realizar o fluxo dos scrapes e gravar os conteudos no DB.

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,7 @@
 import requests
 import time
 from parsel import Selector
+from tech_news.database import create_news
 
 
 # Requisito 1
@@ -13,7 +14,6 @@ def fetch(url):
         response = requests.get(url, timeout=3)
         if response.status_code != 200:
             return None
-        # print(response.text)
         return response.text
     except requests.exceptions.Timeout:
         return None
@@ -23,6 +23,23 @@ def fetch(url):
 def scrape_noticia(html_content):
     """Returns a dict with the selected info from a html content"""
     selector = Selector(text=html_content)
+
+    # scrape writer
+    writer = selector.css(".tec--author__info__link::text").get()
+    if not writer:
+        writer = selector.css(
+            ".tec--article__body-grid div div div div a::text"
+        ).get()
+    if writer == " ":
+        writer = "Equipe TecMundo"
+
+    # scrape shares_count
+    shares_count = selector.css(".tec--toolbar__item::text").get()
+    if not shares_count:
+        shares_count = 0
+    else:
+        shares_count = shares_count.strip().split(" ")[0]
+
     return {
         "url": selector.css('link[rel="canonical"]::attr(href)').get(),
         "title": selector.css("h1#js-article-title::text").get(),
@@ -31,13 +48,8 @@ def scrape_noticia(html_content):
             .xpath("./time/@datetime")
             .get()
         ),
-        "writer": selector.css(".tec--author__info__link::text").get().strip(),
-        "shares_count": int(
-            selector.css(".tec--toolbar__item::text")
-            .get()
-            .strip()
-            .split(" ")[0]
-        ),
+        "writer": writer.strip(),
+        "shares_count": int(shares_count),
         "comments_count": int(
             selector.css("button#js-comments-btn::attr(data-count)").get()
         ),
@@ -81,3 +93,24 @@ def scrape_next_page_link(html_content):
 # Requisito 5
 def get_tech_news(amount):
     """Seu código deve vir aqui"""
+    TECMUNDO_NOVIDADES_URL = "https://www.tecmundo.com.br/novidades"
+    news_urls_list = []
+
+    novidades_html = fetch(TECMUNDO_NOVIDADES_URL)
+    news_urls_list.extend(scrape_novidades(novidades_html))
+
+    while True:
+        if len(news_urls_list) < amount:
+            next_page_url = scrape_next_page_link(novidades_html)
+            novidades_html = fetch(next_page_url)
+            news_urls_list.extend(scrape_novidades(novidades_html))
+        else:
+            break
+
+    news_list = []
+    for news_url in news_urls_list[:amount]:
+        news = scrape_noticia(fetch(news_url))
+        news_list.append(news)
+
+    create_news(news_list)
+    return news_list
