b9116c8dfb058ef6711898de078758312b5c23d4
hugobrr <hugobrr@gmail.com>
2021-09-03 07:47:18 -0300

    o quinto parcial

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -2,6 +2,7 @@ import requests
 from time import sleep
 from parsel import Selector
 from requests.exceptions import Timeout
+from tech_news.database import create_news
 
 
 # Requisito 1
@@ -21,34 +22,38 @@ def fetch(url):
 # Requisito 2
 def scrape_noticia(html_content):
     selector = Selector(text=html_content)
-    url = selector.css("link").xpath("./@href").extract()[-2]
+    url = selector.css("link[rel=canonical]::attr(href)").get() or None
     title = (
         selector.css(".tec--article__header__title").xpath("//h1/text()").get()
-    )
-    timestamp = selector.css("time").xpath("@datetime").get()
-    writer = (
-        selector.css(".tec--author__info__link").xpath("text()").get().strip()
         or None
     )
-    shares_count = (
-        int(
-            selector.css(".tec--toolbar__item")
-            .xpath("text()")
-            .get()
-            .split()[0]
+    timestamp = selector.css("time").xpath("@datetime").get() or None
+    writer = ""
+    writer1 = selector.css(".tec--author__info__link").xpath("text()").get()
+    writer2 = selector.css("a[href*=autor]").xpath("text()").get()
+    if writer1:
+        writer = writer1.strip()
+    elif writer2:
+        writer = writer2
+    else:
+        writer = None
+    shares_count = int(selector.css(".tec--btn::attr(data-count)").get() or 0)
+    comments_count = 0
+    if len(selector.css(".tec--toolbar__item")) > 1:
+        comments_count = (
+            int(
+                selector.css(".tec--toolbar__item")[1]
+                .xpath("//button/text()")
+                .getall()[1]
+                .split()[0]
+            )
+            or 0
         )
-        or 0
+    summary = "".join(
+        selector.css(".tec--article__body > p:first-child")
+        .css("*::text")
+        .getall()
     )
-    comments_count = (
-        int(
-            selector.css(".tec--toolbar__item")[1]
-            .xpath("//button/text()")
-            .getall()[1]
-            .split()[0]
-        )
-        or 0
-    )
-    summary = "".join(selector.xpath("//p")[2].css("*::text").getall())
     len_badge = len(selector.css(".tec--badge").xpath("text()").getall())
     len_primary = len(
         selector.css(".tec--badge--primary").xpath("text()").getall()
@@ -74,6 +79,18 @@ def scrape_noticia(html_content):
         "sources": sources,
         "categories": categories,
     }
+    if not result:
+        result = {
+            "url": None,
+            "title": None,
+            "timestamp": None,
+            "writer": None,
+            "shares_count": 0,
+            "comments_count": 0,
+            "summary": None,
+            "sources": [],
+            "categories": [],
+        }
     return result
 
 
@@ -84,7 +101,7 @@ def scrape_novidades(html_content):
         selector.css(".tec--main")
         .css(".tec--card__title__link")
         .xpath("./@href")
-        .getall()[:20]
+        .getall()
     )
     return list_links
 
@@ -98,5 +115,21 @@ def scrape_next_page_link(html_content):
 
 # Requisito 5
 def get_tech_news(amount):
-    # selector = Selector(text=html_content)
-    """Seu código deve vir aqui"""
+    # print(amount)
+    page_n = fetch("https://www.tecmundo.com.br/novidades")
+    # print("https://www.tecmundo.com.br/novidades")
+    index = 0
+    result_info = []
+    while amount > 0:
+        if index > 19:
+            print(scrape_next_page_link(page_n))
+            page_n = fetch(scrape_next_page_link(page_n))
+            index = 0
+        list_news = scrape_novidades(page_n)
+        read_news = scrape_noticia(fetch(list_news[index]))
+        result_info.append(read_news)
+        # print(list_news[index])
+        # print(index, amount)
+        index += 1
+        amount -= 1
+    create_news(result_info)
