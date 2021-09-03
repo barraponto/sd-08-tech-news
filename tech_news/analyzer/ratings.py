2f14496eba4477f1c74704b86cee7b97419f2f7f
albertassi88 <albertassi88@gmail.com>
2021-09-03 17:56:24 -0300

    Requisito 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,24 @@
+from tech_news.database import find_news
+from operator import itemgetter
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    list_news = []
+    search = find_news()
+    if len(search) == 0:
+        return search
+    for unity_new in search:
+        unity_new["popularity"] = int(unity_new["shares_count"]) + int(
+            unity_new["comments_count"]
+        )
+        list_news.append(unity_new)
+    top_five = sorted(
+        list_news,
+        key=itemgetter("popularity"),
+        reverse=True,
+    )[:5]
+    return [(news["title"], news["url"]) for news in top_five]
 
 
 # Requisito 11
