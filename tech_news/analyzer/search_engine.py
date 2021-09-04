8e8b90733359bf18793164528c7d8f170450c652
Rodrigo de Souza <rdesouza2008@gmail.com>
2021-09-04 12:16:24 -0300

    implementa search_by_date

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,19 +1,32 @@
 from tech_news.database import search_news
+from datetime import datetime
 
 
 # Requisito 6
 def search_by_title(title):
     news = search_news({"title": {"$regex": title, "$options": "i"}})
     print(news)
-    return [
-        (actual_news["title"], actual_news["url"])
-        for actual_news in news
-    ]
+    return [(actual_news["title"], actual_news["url"]) for actual_news in news]
+
+
+def date_is_valid(date):
+    try:
+        datetime.strptime(date, "%Y-%m-%d")
+        return True
+    except ValueError:
+        return False
 
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        datetime.strptime(date, "%Y-%m-%d")
+        news = search_news({"timestamp": {"$regex": date, "$options": "i"}})
+        return [
+            (actual_news["title"], actual_news["url"]) for actual_news in news
+        ]
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
