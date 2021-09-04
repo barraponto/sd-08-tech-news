2b7aa598a44a1fd88fd22d22e90b4c54fee8d572
Edson Araujo de Barros Junior <edson@pop-os.localdomain>
2021-09-04 19:59:09 -0300

    Req 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,27 @@
+from tech_news.database import db
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    results = db.news.aggregate([
+        {
+            "$project": {
+                "title": 1, "url": 1, "popularity": {
+                    "$add": ["$shares_count", "$comments_count"]
+                    }
+                }
+        },
+        {
+            "$sort": {"popularity": -1, "title": 1}
+        },
+        {
+            "$limit": 5
+        }
+
+    ])
+
+    return [(result["title"], result["url"])
+            for result in list(results)]
 
 
 # Requisito 11
