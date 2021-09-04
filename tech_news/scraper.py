e42c297e5e7378dfd87f808b8dc3dbe14d0dc744
Ivanildo Cândido <systechtecnologia@gmail.com>
2021-09-03 21:53:46 -0300

    Enviando requisito 4

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -52,7 +52,8 @@ def scrape_novidades(html_content):
 
 # Requisito 4
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    return selector.css(".tec--btn::attr(href)").get()
 
 
 # Requisito 5
