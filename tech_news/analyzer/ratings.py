625cc2ded9371d2a0eec4d20eed5589057930b18
Fabio Ferreira <fabio35547@gmail.com>
2021-09-04 10:26:35 -0300

    Requisito 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,24 @@
+from tech_news.database import find_news
+
+
+def by_popularity(new):
+    return new['shares_count'] + new['comments_count']
+
+
+def format_output_new(idx, new):
+    return (f'noticia_{idx}',  new['url'])
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news = find_news()
+    news.sort(key=by_popularity, reverse=True)
+
+    # http://devfuria.com.br/python/built-in-enumerate/
+    return [
+        format_output_new(idx, new)
+        for idx, new in enumerate(news, start=1) if idx <= 5
+    ]
 
 
 # Requisito 11
