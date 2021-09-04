757a67ce352a4ce5a11623598610d650f554c973
Fabio Ferreira <fabio35547@gmail.com>
2021-09-04 09:55:11 -0300

    Requisito 7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,12 +1,23 @@
 from tech_news.database import search_news
+from datetime import datetime
 
 
 def format_news(new):
     return (new['title'], new['url'])
 
 
+def is_valid_date(date):
+    try:
+        # https://www.alura.com.br/artigos/lidando-com-datas-e-horarios-no-python?gclid=Cj0KCQjwssyJBhDXARIsAK98ITRxkPnQnvWJoiebq-w-4ErT4HX-gL8qLA6jD7hG9N7sXox8TKfkqv0aAjczEALw_wcB
+        datetime.strptime(date, "%Y-%m-%d")
+        return True
+    except ValueError:
+        return False
+
+
 # Requisito 6
 def search_by_title(title):
+    # https://docs.mongodb.com/manual/reference/operator/query/regex/
     query = {'title': {'$regex': f'.*{title}.*', '$options': "i"}}
     news = search_news(query)
 
@@ -15,7 +26,14 @@ def search_by_title(title):
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    if is_valid_date(date) is False:
+        raise ValueError("Data inválida")
+    else:
+        # https://docs.mongodb.com/manual/reference/operator/query/regex/
+        search_news_query = {"timestamp": {"$regex": date, "$options": "i"}}
+        news = search_news(search_news_query)
+
+        return [format_news(new) for new in news]
 
 
 # Requisito 8
