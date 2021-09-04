584c0d302a9c1f76d443ce6ac1b7b41a70b40c79
victor_felipe <victorfelipeoliveira1@gmail.com>
2021-09-03 21:23:30 -0300

    req 1-2

---
+++ tech_news/my_Test.py
@@ -0,0 +1,31 @@
+from scraper import fetch, scrape_noticia
+import json
+from parsel import Selector
+
+expexted = ""
+with open("tests/assets/cached_news.json") as arquivo:
+    all_news = json.load(arquivo)
+    expexted = all_news[15]
+
+htmls2 = fetch(
+    str(
+        "https://www.tecmundo.com.br/dispositivos-moveis/215327-pixel-5a-tera-lancamento-limitado-devido-escassez-chips.htm"
+    )
+)
+
+result1 = scrape_noticia(htmls2)
+
+for k in expexted:
+    print(k, " -- ", expexted[k])
+    print(k, " -- ", result1[k], end="\n\n")
+
+print(end="\n\n")
+selector1 = Selector(text=htmls2)
+
+summary = (
+    selector1.xpath("//div[contains(@class, 'tec--article__body')][1]/p[1]")
+    .css("*::text")
+    .getall()
+)
+
+print(summary)
--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,11 +1,95 @@
-# Requisito 1
+import time
+import requests
+from parsel import Selector
+import json
+import re
+
+
 def fetch(url):
-    """Seu código deve vir aqui"""
+    try:
+        time.sleep(1)
+        html = requests.get(url, timeout=3)
+        if html.status_code == 200:
+            return html.text
+        else:
+            return None
+    except requests.ReadTimeout:
+        return None
+
+
+def replace_space(data):
+    if len(data) > 0:
+        for x in range(len(data)):
+            data[x] = data[x][1:-1]
+    else:
+        data = data[1::-2]
 
 
 # Requisito 2
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+
+    url = (
+        selector.xpath(
+            "//meta[contains(@content, 'https://www.tecmundo.com.br/')]"
+        )
+        .css("::attr(content)")
+        .get()
+    )
+
+    title = selector.css("#js-article-title::text").get()
+
+    timestamp = selector.css("div time::attr(datetime)").get()
+
+    writer = json.loads(
+        (
+            selector.xpath(
+                str("//script[contains(@type, 'application/ld+json')]")
+            )
+            .css("::text")
+            .getall()[1]
+        )
+    )["author"]["name"]
+
+    shares_count = re.sub(
+        r"[A-Za-z]", "", selector.css(".tec--toolbar__item::text").get()
+    )
+
+    comments_count = selector.css(
+        ".tec--toolbar__item button::attr(data-count)"
+    ).get()
+
+    summary = (
+        selector.xpath("//div[contains(@class, 'tec--article__body')][1]/p[1]")
+        .css("*::text")
+        .getall()
+    )
+
+    str_concat = ""
+    for world in summary:
+        str_concat += world
+
+    sources = selector.css(".z--mb-16 div a::text").getall()
+
+    replace_space(sources)
+
+    categories = selector.css("#js-categories a::text").getall()
+
+    replace_space(categories)
+
+    data = {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "shares_count": int(shares_count.replace(" ", "")) or 0,
+        "comments_count": int(comments_count) or 0,
+        "summary": str_concat,
+        "sources": sources,
+        "categories": categories,
+    }
+
+    return data
 
 
 # Requisito 3
