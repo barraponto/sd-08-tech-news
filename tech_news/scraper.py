b5e107a6234ac901ec9682797ca2a0447dd5632e
Ivanildo Cândido <systechtecnologia@gmail.com>
2021-09-03 21:37:48 -0300

    Enviando requisito 2

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,5 +1,6 @@
 import time
 import requests
+from parsel import Selector
 
 
 # Requisito 1
@@ -16,7 +17,31 @@ def fetch(url):
 
 # Requisito 2
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    return {
+        "url": selector.css("head link[rel=canonical]::attr(href)").get(),
+        "title": selector.css(".tec--article__header__title::text").get(),
+        "timestamp": selector.css("#js-article-date::attr(datetime)").get(),
+        "writer": selector.css(".tec--author__info__link::text").get().strip(),
+        "shares_count": int(
+            selector.css(".tec--toolbar__item::text").get().split(" ")[1]
+        ),
+        "comments_count": int(
+            selector.css("#js-comments-btn::attr(data-count)").get()
+        ),
+        "summary": "".join(
+            selector.css(
+                ".tec--article__body p:first-of-type *::text"
+            ).getall()
+        ),
+        "sources": [
+            i.strip()
+            for i in selector.css("[class='tec--badge']::text").getall()
+        ],
+        "categories": [
+            i.strip() for i in selector.css("#js-categories a::text").getall()
+        ],
+    }
 
 
 # Requisito 3
