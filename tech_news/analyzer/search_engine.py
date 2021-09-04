d55f8ea76b931d5a222b1f76ac42d11af56ac14f
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-09-04 10:45:48 -0300

    extrai função get_result_list e implementa parcialmente search_by_date - requisito 07

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,5 +1,14 @@
 import re
+import datetime
 from tech_news.database import search_news
+
+
+def get_result_list(list_to_format):
+    news_tuple_list = []
+    for news in list_to_format:
+        title_url_tuple = (news["title"], news["url"])
+        news_tuple_list.append(title_url_tuple)
+    return news_tuple_list
 
 
 # Requisito 6
@@ -7,16 +16,16 @@ def search_by_title(title):
     """Seu código deve vir aqui"""
     query = re.compile(f"{title}", re.IGNORECASE)
     news_by_title = search_news({"title": query})
-    news_tuple_list = []
-    for news in news_by_title:
-        title_url_tuple = (news["title"], news["url"])
-        news_tuple_list.append(title_url_tuple)
-    return news_tuple_list
+    return get_result_list(news_by_title)
 
 
 # Requisito 7
 def search_by_date(date):
     """Seu código deve vir aqui"""
+    year, month, day = date.split('-')
+    date_query = datetime.date(int(year), int(month), int(day))
+    news_by_date = search_news({"timestamp": date_query})
+    return get_result_list(news_by_date)
 
 
 # Requisito 8
