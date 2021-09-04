52c0f9a65eae362cfe27b815db596e85f2488ac6
Paulo Simões <paulohbsimoes@gmail.com>
2021-09-04 16:35:41 -0300

    feat: add req 4

--- tech_news/NewsScraper.py
+++ tech_news/NewsScraper.py
@@ -9,3 +9,8 @@ class NewsScraper:
         return self.selector.css(
             ".tec--list .tec--card__thumb__link::attr(href)"
         ).getall()
+
+    def get_next_page_url(self):
+        return self.selector.xpath(
+            '//a[re:match(text(), "Mostrar mais notícias")]/@href'
+        ).get()
--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -36,6 +36,7 @@ def scrape_novidades(html_content):
 # Requisito 4
 def scrape_next_page_link(html_content):
     """Seu código deve vir aqui"""
+    return NewsScraper(html_content).get_next_page_url()
 
 
 # Requisito 5
