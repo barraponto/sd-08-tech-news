d07652237c7125ecc2e106c9145916062876c66d
Ivanildo Cândido <systechtecnologia@gmail.com>
2021-09-05 16:37:33 -0300

    Enviando requisito 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,28 @@
+from tech_news.database import db
+
+
 # Requisito 10
+def search_top_news(qtd):
+    return list(
+        db.news.aggregate(
+            [
+                {
+                    "$addFields": {
+                        "popularity": {
+                            "$sum": ["$shares_count", "$comments_count"]
+                        }
+                    }
+                },
+                {"$sort": {"popularity": -1}},
+                {"$limit": qtd},
+                {"$project": {"_id": 0, "url": 1, "title": 1}},
+            ]
+        )
+    )
+
+
 def top_5_news():
-    """Seu código deve vir aqui"""
+    return [(news["title"], news["url"]) for news in search_top_news(5)]
 
 
 # Requisito 11
