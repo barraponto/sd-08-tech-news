8c86e82ba6c0aa55a740bf8a45696df37f41cd19
Bruno Wesley <bruno.wesley77@gmail.com>
2021-09-05 14:37:56 -0300

    req 11

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,8 @@
+from tech_news.database import find_news
+from collections import Counter
+import itertools
+
+
 # Requisito 10
 def top_5_news():
     """Seu código deve vir aqui"""
@@ -6,3 +11,13 @@ def top_5_news():
 # Requisito 11
 def top_5_categories():
     """Seu código deve vir aqui"""
+    responses = find_news()
+    categories_list = []
+    for response in responses:
+        categories_list.append(response["categories"])
+    totals = Counter(
+        i for i in list(itertools.chain.from_iterable(categories_list))
+    )
+    totals_sorted = sorted(totals)
+    limit = slice(5)
+    return totals_sorted[limit]
