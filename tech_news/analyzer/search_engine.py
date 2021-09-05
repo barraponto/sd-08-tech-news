82b571a733c7c4dfa944eeaab5a4f62b48f7bc4a
emassaki <massakihirayama@gmail.com>
2021-09-05 16:37:22 -0300

    req 7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 import tech_news.database as db
+from datetime import datetime
 
 
 # Requisito 6
@@ -13,8 +14,23 @@ def search_by_title(title):
 
 
 # Requisito 7
+def validate_date(date):
+    try:
+        datetime.strptime(date, "%Y-%m-%d")
+    except ValueError:
+        raise ValueError("Data inválida")
+
+
 def search_by_date(date):
     """Seu código deve vir aqui"""
+    validate_date(date)
+    found_news = db.find_news()
+    return [
+        (new["title"], new["url"])
+        for new in found_news
+        if datetime.strptime(date, "%Y-%m-%d").date()
+        == datetime.strptime(new["timestamp"], "%Y-%m-%dT%H:%M:%S").date()
+    ]
 
 
 # Requisito 8
