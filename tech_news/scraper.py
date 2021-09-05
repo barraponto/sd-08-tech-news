b082b29d94a81c6815405e64a014757c34b9bcd3
Tandy <tandyfw@gmail.com>
2021-09-05 14:27:54 -0300

    Requisito 2 concluido

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,3 +1,4 @@
+from parsel import Selector
 import time
 import requests
 
@@ -13,8 +14,52 @@ def fetch(url):
 
 
 # Requisito 2
+base_url = 'https://www.tecmundo.com.br'
+def filter_url(link):
+    return base_url in link
+
+
+def find_url(html_content):
+    selector = Selector(text=html_content)
+    href_links = selector.css('head link::attr(href)').getall()
+    return list(filter(filter_url, href_links))[-1]
+
+
+def summary_serializer(summary):
+    summary_selector = Selector(text=str(summary))
+    summary_text = summary_selector.css("p ::text").getall()
+    return "".join(summary_text[1:])
+
+
+def remove_white_spaces(array):
+    newArray = []
+    for item in array:
+        newArray.append(item[1:-1])
+    return newArray
+
+
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    url = find_url(html_content)
+    shares = selector.css("div.tec--toolbar__item::text").get().split(' ')[1]
+    comments = selector.css("#js-comments-btn::text").get().split(' ')[1]
+    summary = selector.css("div.tec--article__body p").get(),
+    str_summary = summary_serializer(summary)
+    sources = selector.css("div.z--mb-16 div a::text").getall()
+    categories = selector.css("#js-categories a::text").getall()
+
+    info_news = {
+        "url": url,
+        "title": selector.css("#js-article-title::text").get(),
+        "timestamp": selector.css("div.tec--timestamp--lg div time::attr(datetime)").get(),
+        "writer": selector.css(".tec--author__info__link::text").get()[1:-1],
+        "shares_count": int(shares) if shares !='' else 0,
+        "comments_count": int(comments) if comments !='' else 0,
+        "summary": str_summary,
+        "sources": remove_white_spaces(sources),
+        "categories": remove_white_spaces(categories),
+    }
+    return info_news
 
 
 # Requisito 3
