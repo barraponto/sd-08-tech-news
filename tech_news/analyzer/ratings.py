66769cec19e137f5192042d370174b42c1cd7f04
emassaki <massakihirayama@gmail.com>
2021-09-05 17:43:33 -0300

    req 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,24 @@
+import tech_news.database as db
+
+
 # Requisito 10
 def top_5_news():
     """Seu código deve vir aqui"""
+    found_news = db.find_news()
+    news_popularity = [
+        {
+            "title": new["title"],
+            "url": new["url"],
+            "popularity": new["comments_count"] + new["shares_count"],
+        }
+        for new in found_news
+    ]
+    sorted_news_by_popularity = sorted(
+        news_popularity, key=lambda news: news["popularity"],
+        reverse=True
+    )
+    most_popular_news = sorted_news_by_popularity[:5]
+    return [(new['title'], new['url']) for new in most_popular_news]
 
 
 # Requisito 11
