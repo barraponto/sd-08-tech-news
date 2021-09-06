053375d0dcd12268f4aa9acf392fe714c0adedf6
Ana Karine <anakarine.ti@gmail.com>
2021-09-06 20:30:46 -0300

    def search_by_title

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,18 @@
+import tech_news.database as db
+
+
 # Requisito 6
 def search_by_title(title):
     """Seu código deve vir aqui"""
+    news = db.find_news()
+    result = []
+
+    for info in news:
+        if info["title"].lower() == title.lower():
+            result.append(
+                (info["title"], info["url"])
+            )
+    return result
 
 
 # Requisito 7
