c6c263130dde9e08e1bd715ac4d317c67a49a26d
Tiago <tiagaoalb@gmail.com>
2021-09-06 11:22:43 -0300

    add 6th requirement

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,16 @@
+import tech_news.database as db
+from datetime import datetime
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    find_tech_news = db.find_news()
+    result = [
+        (tech_news["title"], tech_news["url"])
+        for tech_news in find_tech_news
+        if title.lower() in tech_news["title"].lower()
+    ]
+    return result
 
 
 # Requisito 7
