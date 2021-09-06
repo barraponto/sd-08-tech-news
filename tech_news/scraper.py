e764ac8ac6bcccb66a0968f2713355987260b58e
lucasbgama <lucasbg@protonmail.com>
2021-09-06 01:30:48 -0300

    initial commit: primeiro e segundo requisitos

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,11 +1,42 @@
+import requests
+import time
+from parsel import Selector
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    time.sleep(1)
+    try:
+        response = requests.get(url, timeout=3)
+        if response.status_code != 200:
+            return None
+        return response.text
+    except requests.ReadTimeout:
+        return None
 
 
 # Requisito 2
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    url = selector.css("link[rel=canonical]::attr(href)").get()
+    title = selector.css("#js-article-title::text").get()
+    timestamp = selector.css("#js-article-date::attr(datetime)").get()
+    writer = selector.css("#js-author-bar div a::text").get().strip()
+    shares_count = selector.css("tec--toolbar__item :last-child::text") \
+        .re_first(r"\d+", default=0)
+    comments_count = selector.css("#js-comments-btn :last-child::text") \
+        .re_first(r"\d+", default=0)
+    summary = ''.join(selector
+                      .css(".tec--article__body p:first-child *::text")
+                      .getall())
+    sources = selector.css("div:not(#js-categories) > a.tec--badge::text") \
+                      .re(r"\S.*\S")
+    categories = selector.css("#js-categories a::text").re(r"\S.*\S")
+    return {"url": url, "title": title, "timestamp": timestamp,
+            "writer": writer,
+            "shares_count": shares_count, "comments_count": comments_count,
+            "summary": summary,
+            "sources": sources, "categories": categories}
 
 
 # Requisito 3
