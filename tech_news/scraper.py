0a895df58a0bbd4a81c61dd4f8684428c86ed1f6
Digo-Castro <digo_castro@outlook.com>
2021-09-06 15:00:06 -0300

    [Req 5]

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,7 @@
 import requests
 import time
 from parsel import Selector
+from tech_news.database import create_news
 
 
 # Requisito 1
@@ -16,15 +17,30 @@ def fetch(url):
 
 
 # Requisito 2
+def get_writer(selector):
+    writer = selector.css(".tec--author__info__link::text").get()
+    if not writer:
+        writer = selector.css(
+            ".tec--timestamp.tec--timestamp--lg a::text"
+        ).get()
+    if not writer:
+        writer = selector.css(".tec--author__info p::text").get()
+    return writer.strip() if writer else None
+
 def scrape_noticia(html_content):
     selector = Selector(text=html_content)
     url = selector.css("link[rel=canonical]::attr(href)").get()
     title = selector.css(".tec--article__header__title::text").get()
     timestamp = selector.css("time::attr(datetime)").get()
-    writer = selector.css(".tec--author__info__link::text").get().strip()
-    shares_count = int(
-        selector.css(".tec--toolbar__item::text").get().strip().split()[0]
-    )
+    writer = get_writer(selector)
+
+    shares_count = selector.css(".tec--toolbar__item::text").get()
+    if (type(shares_count) == str):
+        shares_count = int(shares_count.strip().split()[0])
+    else:
+        shares_count = 0
+    
+
     comments_count = int(
         selector.css("#js-comments-btn ::attr(data-count)").get()
     )
@@ -75,4 +91,19 @@ def scrape_next_page_link(html_content):
 
 # Requisito 5
 def get_tech_news(amount):
-    """Seu código deve vir aqui"""
+    URL = "https://www.tecmundo.com.br/novidades"
+
+    html_contents = fetch(URL)
+    data = []
+
+    while len(data) < amount:
+        for link in scrape_novidades(html_contents):
+            if len(data) < amount:
+                html_news = fetch(link)
+                data.append(scrape_noticia(html_news))
+
+        if len(data) < amount:
+            next_link = scrape_next_page_link(html_contents)
+            html_contents = fetch(next_link)
+    create_news(data)
+    return data
