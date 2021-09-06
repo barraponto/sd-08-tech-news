76fb251c2c4996c3817ebef7dd9642cbae551d41
Digo-Castro <digo_castro@outlook.com>
2021-09-06 15:50:54 -0300

    [Reqs 6 a 9]

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,18 +1,51 @@
+from datetime import datetime
+from tech_news.database import search_news
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    query = search_news({"title": {"$regex": title, "$options": "i"}})
+    search_results = list()
+
+    for item in query:
+        search_results.append((item["title"], item["url"]))
+
+    return search_results
 
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        datetime.strptime(date, "%Y-%m-%d")
+    except ValueError:
+        raise ValueError("Data inválida")
+
+    query = search_news({"timestamp": {"$regex": date}})
+    search_results = list()
+
+    for item in query:
+        search_results.append((item["title"], item["url"]))
+
+    return search_results
 
 
 # Requisito 8
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    query = search_news({"sources": {"$regex": source, "$options": "i"}})
+    search_results = list()
+
+    for item in query:
+        search_results.append((item["title"], item["url"]))
+
+    return search_results
 
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    query = search_news({"categories": {"$regex": category, "$options": "i"}})
+    search_results = list()
+
+    for item in query:
+        search_results.append((item["title"], item["url"]))
+
+    return search_results
