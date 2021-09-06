c14b985546d0f0d9451a7a07cb1ccd62de5e37fd
Seu nome <luiza.dilly@gmail.com>
2021-09-06 17:17:08 -0300

    req suficiente pros 80

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,7 +1,8 @@
-# Requisito 6
 import tech_news.database as db
+from datetime import datetime
 
 
+# Requisito 6
 def search_by_title(title):
     news = db.find_news()
     list = []
@@ -14,14 +15,40 @@ def search_by_title(title):
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    news = db.find_news()
+    try:
+        datetime.strptime(date, "%Y-%m-%d").date()
+        list = []
+        for x in news:
+            if datetime.strptime(date, "%Y-%m-%d").date() == datetime.strptime(
+                x['timestamp'], "%Y-%m-%dT%H:%M:%S"
+            ).date():
+                result = (x["title"], x["url"])
+                list.append(result)
+            return list
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    news = db.find_news()
+    source_list = []
+    for x in news:
+        for y in x["sources"]:
+            if y.lower() == source.lower():
+                result = (x["title"], x["url"])
+                source_list.append(result)
+    return source_list
 
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    news = db.find_news()
+    cat_list = []
+    for x in news:
+        for y in x["categories"]:
+            if y.lower() == category.lower():
+                result = (x["title"], x["url"])
+                cat_list.append(result)
+    return cat_list
