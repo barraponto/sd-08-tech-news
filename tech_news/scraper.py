0f4d7b4a35b09c4eda3218aa75253a97ee9ca832
Ana Karine <anakarine.ti@gmail.com>
2021-09-06 00:24:28 -0300

    def scrape_noticia

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,5 +1,12 @@
+from parsel import Selector
 import requests
 import time
+
+# Referências:
+# http://www.dannejaha.se/programming/google-and-seo/Na7d08fa2_cannonical/
+# https://www.tabnine.com/code/java/packages/com.chimbori.crux.common
+# https://www.w3schools.com/python/ref_string_strip.asp
+# https://developer.mozilla.org/pt-BR/docs/Web/CSS/:nth-child
 
 
 # Requisito 1
@@ -17,6 +24,46 @@ def fetch(url):
 # Requisito 2
 def scrape_noticia(html_content):
     """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+
+    writer = selector.css(".tec--author__info__link::text").get()
+    if writer:
+        writer = writer.strip()
+    else:
+        writer = None
+
+    shares_count = selector.css(".tec--toolbar__item::text").get().split()[0]
+    if shares_count:
+        shares_count = int(shares_count)
+    else:
+        shares_count = 0
+
+    comments_count = selector.css(".tec--btn::text").get().split(" ")[0]
+    if comments_count:
+        comments_count = int(comments_count)
+    else:
+        comments_count = 0
+
+    summary = selector.css(
+        ".tec--article__body p:nth-child(1) ::text"
+    ).getall()
+
+    sources = selector.css(".z--mb-16 .tec--badge::text").getall()
+
+    categories = selector.css("#js-categories .tec--badge::text").getall()
+
+    info = {
+        "url": selector.css("head link[rel=canonical]::attr(href)").get(),
+        "title": selector.css(".tec--article__header__title::text").get(),
+        "timestamp": selector.css("#js-article-date::attr(datetime)").get(),
+        "writer": writer,
+        "shares_count": shares_count,
+        "comments_count": comments_count,
+        "summary": "".join(summary),
+        "sources": [source.strip() for source in sources],
+        "categories": [categorie.strip() for categorie in categories]
+    }
+    return info
 
 
 # Requisito 3
