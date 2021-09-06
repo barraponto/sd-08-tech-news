e623886fb84af7f771c36912bc80469b3f4db8ab
lucasbgama <lucasbg@protonmail.com>
2021-09-06 02:58:38 -0300

    req 5

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,9 @@
 import requests
 import time
 from parsel import Selector
+from tech_news.database import create_news
+
+BASE_URL = "https://www.tecmundo.com.br/novidades"
 
 
 # Requisito 1
@@ -21,13 +24,15 @@ def scrape_noticia(html_content):
     url = selector.css("link[rel=canonical]::attr(href)").get()
     title = selector.css("#js-article-title::text").get()
     timestamp = selector.css("#js-article-date::attr(datetime)").get()
-    writer = selector.css("#js-author-bar div a::text").get().strip()
-    shares_count = selector.css("tec--toolbar__item :last-child::text") \
-        .re_first(r"\d+", default=0)
-    comments_count = selector.css("#js-comments-btn :last-child::text") \
-        .re_first(r"\d+", default=0)
+    writer = selector.css(".tec--article__body-grid .z--font-bold *::text") \
+        .get().strip()
+    shares_count = int(selector
+                       .css(".tec--toolbar__item:not(:last-child) *::text")
+                       .re_first(r"\d+", default=0))
+    comments_count = int(selector.css("#js-comments-btn *::text")
+                         .re_first(r"\d+", default=0))
     summary = ''.join(selector
-                      .css(".tec--article__body p:first-child *::text")
+                      .css(".tec--article__body > p:first-child *::text")
                       .getall())
     sources = selector.css("div:not(#js-categories) > a.tec--badge::text") \
                       .re(r"\S.*\S")
@@ -53,4 +58,15 @@ def scrape_next_page_link(html_content):
 
 # Requisito 5
 def get_tech_news(amount):
-    """Seu código deve vir aqui"""
+    news = []
+    next_page = BASE_URL
+    while (len(news) < amount and next_page):
+        resp = fetch(next_page)
+        novidades = scrape_novidades(resp)[0:amount - len(news)]
+        novidades_html = [fetch(noticia) for noticia in novidades]
+        curr_page_news = [scrape_noticia(novidade_html)
+                          for novidade_html in novidades_html]
+        news.extend(curr_page_news)
+        next_page = scrape_next_page_link(resp)
+    create_news(news)
+    return news
