336b881e3d9e093be01f33819460d968255e86b1
lucasbgama <lucasbg@protonmail.com>
2021-09-06 04:06:05 -0300

    requisitos 6 e 7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,11 +1,21 @@
+from datetime import datetime
+from tech_news.database import search_news
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    results = search_news({"title": {"$regex": title, "$options": "i"}})
+    return [(result["title"], result["url"]) for result in results]
 
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        datetime.strptime(date, "%Y-%m-%d")
+    except ValueError:
+        raise ValueError("Data inválida")
+    results = search_news({"timestamp": {"$regex": date}})
+    return [(result["title"], result["url"]) for result in results]
 
 
 # Requisito 8
