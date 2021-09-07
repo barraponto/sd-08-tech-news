f0780b98c5501a9deff8cceca2bf45ed0c7515d0
victor_felipe <victorfelipeoliveira1@gmail.com>
2021-09-07 01:30:29 -0300

    fix req 11-12'

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -79,25 +79,23 @@ def analyzer_menu():
     opc = ""
     EXIT = False
 
-    while not EXIT:
-        print(
-            str(
-                "Selecione uma das opções a seguir:\n "
-                "0 - Popular o banco com notícias;\n "
-                "1 - Buscar notícias por título;\n "
-                "2 - Buscar notícias por data;\n "
-                "3 - Buscar notícias por fonte;\n "
-                "4 - Buscar notícias por categoria;\n "
-                "5 - Listar top 5 notícias;\n "
-                "6 - Listar top 5 categorias;\n "
-                "7 - Sair."
-            )
+    print(
+        str(
+            "Selecione uma das opções a seguir:\n "
+            "0 - Popular o banco com notícias;\n "
+            "1 - Buscar notícias por título;\n "
+            "2 - Buscar notícias por data;\n "
+            "3 - Buscar notícias por fonte;\n "
+            "4 - Buscar notícias por categoria;\n "
+            "5 - Listar top 5 notícias;\n "
+            "6 - Listar top 5 categorias;\n "
+            "7 - Sair."
         )
-        opc = str(input())
-        EXIT = switch_case(opc)
-        if EXIT:
-            print("Encerrando script")
-            break
+    )
+    opc = str(input())
+    EXIT = switch_case(opc)
+    if EXIT:
+        print("Encerrando script")
 
 
 if __name__ == "__main__":
