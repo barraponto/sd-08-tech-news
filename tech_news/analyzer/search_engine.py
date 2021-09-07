284d692c381604ca85c4710ac644fe73ce7f803f
Jeferson Schimuneck <jefersonschimuneck@gmail.com>
2021-09-06 23:24:49 -0300

    feat: complete requirements 7, 8, 9

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,18 +1,42 @@
-# Requisito 6
+import re
+import datetime
+from tech_news.database import db
+
+
+# https://stackoverflow.com/questions/6266555/querying-mongodb-via-pymongo-in-case-insensitive-efficiently
+
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    news_response = db.news.find({
+        'title': {'$regex': re.compile(title, re.IGNORECASE)}
+    })
+
+    return [(news['title'], news['url']) for news in news_response]
 
 
-# Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        datetime.datetime.strptime(date, '%Y-%m-%d')
+    except ValueError:
+        raise ValueError('Data inválida')
+
+    news_response = db.news.find({
+        'timestamp': {'$regex': re.compile(date)}
+    })
+
+    return [(news['title'], news['url']) for news in news_response]
 
 
-# Requisito 8
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    news_response = db.news.find({
+        'sources': {'$regex': re.compile(source, re.IGNORECASE)}
+    })
+
+    return [(news['title'], news['url']) for news in news_response]
 
 
-# Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    news_response = db.news.find({
+        'categories': {'$regex': re.compile(category, re.IGNORECASE)}
+    })
+
+    return [(news['title'], news['url']) for news in news_response]
