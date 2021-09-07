1f8b75cac395ce5f6851278feca558111b324a9c
Anderson Santana <anderslash@gmail.com>
2021-09-07 16:53:08 -0300

    refactor: requisito 2

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -17,35 +17,37 @@ def fetch(url):
 
 
 # Requisito 2
+def get_writer(selector):
+    writer = selector.css(
+        '.tec--author__info__link::text').get()
+    if not writer:
+        writer = selector.css(
+            '.tec--timestamp__item.z--font-bold a::text').get()
+    if not writer:
+        writer = selector.css(
+            '.tec--author__info p::text').get()
+    return writer.strip() if writer else None
+
+
+def get_shares_count(selector):
+    try:
+        return int(
+            selector.css('.tec--toolbar__item::text').get()
+            .replace("Compartilharam", "").strip())
+    except AttributeError:
+        return 0
+
+
 def scrape_noticia(html_content):
     """Given HTML content, returns an object with selected data"""
     selector = Selector(html_content)
-
-    def get_writer():
-        writer = selector.css(
-            '.tec--author__info__link::text').get()
-        if not writer:
-            writer = selector.css(
-                '.tec--timestamp__item.z--font-bold a::text').get()
-        if not writer:
-            writer = selector.css(
-                '.tec--author__info p::text').get()
-        return writer.strip() if writer else None
-
-    def get_shares_count():
-        try:
-            return int(
-                selector.css('.tec--toolbar__item::text').get()
-                .replace("Compartilharam", "").strip())
-        except AttributeError:
-            return 0
 
     news_object = {
         'url': selector.css('link[rel=canonical]::attr(href)').get(),
         'title': selector.css('#js-article-title::text').get(),
         'timestamp': selector.css('#js-article-date::attr(datetime)').get(),
-        'writer': get_writer(),
-        'shares_count': get_shares_count(),
+        'writer': get_writer(selector),
+        'shares_count': get_shares_count(selector),
         'comments_count': int(
             selector.css('#js-comments-btn::attr(data-count)').get()),
         'summary': ''.join(selector.css(
