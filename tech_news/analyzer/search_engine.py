26824ecce1629518cf8f7630e47ff3f51b471987
Tandy <tandyfw@gmail.com>
2021-09-07 09:11:05 -0300

    Requisito 7 concluido

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timezone
 from tech_news.database import db
 
 
@@ -19,7 +20,24 @@ def search_by_title(title):
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        formated_Date = datetime.strptime(date, "%Y-%m-%d")
+        year = formated_Date.strftime("%Y")
+        month = formated_Date.strftime("%m")
+        day = formated_Date.strftime("%d")
+        datetime(int(year), int(month), int(day))
+        result = list(db.news.find(
+            {"timestamp": {"$regex": date}},
+            {"_id": 0, "title": 1, "url": 1},
+        ))
+        if len(result) == 0:
+            return result
+        else:
+            tuplas = result[0] if result is not None else []
+            lista = list(tuple(tuplas.values()))
+            return [(lista[1], lista[0])]
+    except:
+        raise ValueError('Data inválida')
 
 
 # Requisito 8
