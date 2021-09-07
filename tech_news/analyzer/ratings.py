25d0eaec5e01cf60fe2665bb7d90676d3dcc682e
Ana Higo <anahigo.arq@gmail.com>
2021-09-07 16:07:59 -0300

    Teste Local nao esta funcionando comparando projeto turma 6 com turma 8, requisitos basicos

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,8 +1,43 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
     """Seu código deve vir aqui"""
+    news = find_news()
+    popularity_list = []
+    result = []
+
+    if len(news) > 0:
+        for dic_news in news:
+            popularity = dic_news["shares_count"] + dic_news["comments_count"]
+            dic_news["popularity"] = popularity
+            popularity_list.append(dic_news)
+        # https://stackoverflow.com/questions/72899/how-do-i-sort-a-list-of-dictionaries-by-a-value-of-the-dictionary
+        popularity_list.sort(
+            key=lambda x: (x["popularity"], x["title"]), reverse=True)
+        for news in popularity_list[:5]:
+            result.append((news["title"], news["url"]))
+        return result
+    else:
+        return []
 
 
 # Requisito 11
 def top_5_categories():
     """Seu código deve vir aqui"""
+    news = find_news()
+    list_categories = []
+    count_categories = []
+    result = []
+    for dic_news in news:
+        list_categories.extend(dic_news["categories"])
+    for item in list_categories:
+        contador = list_categories.count(item)
+        dict_count = {"item": item, "count": contador}
+        if dict_count not in count_categories:
+            count_categories.append(dict_count)
+    count_categories.sort(key=lambda x: (x["count"], x["item"]))
+    for item in count_categories[:5]:
+        result.append(item["item"])
+    return result
--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,23 +1,107 @@
+import requests
+import time
+from parsel import Selector
+from tech_news.database import create_news
+
+
 # Requisito 1
 def fetch(url):
-    """alteração"""
+    """Seu código deve vir aqui"""
+    try:
+        response = requests.get(url, timeout=3)
+        time.sleep(1)
+        if(response.status_code != 200):
+            return None
+        return response.text
+    except requests.ReadTimeout:
+        return None
 
 
 # Requisito 2
 def scrape_noticia(html_content):
     """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    url = selector.css("head [rel='canonical'] ").css("link::attr(href)").get()
+    title_content = selector.css("h1::text").get()
+    timestamp = selector.css("time::attr(datetime)").get()
+    writer = selector.css("a.tec--author__info__link::text").get()
+    if writer is not None:
+        writer = writer.strip()
+    shares = selector.css("div.tec--toolbar__item::text").get()
+    if shares is None:
+        shares = 0
+    else:
+        shares = str(shares).split()[0]
+    coment = selector.css(".tec--toolbar__item button::attr(data-count)").get()
+
+    summary = selector.css("div.tec--article__body p:first-child *::text")
+    summary_content = summary.getall()
+    summary_join = "".join(str(item) for item in summary_content)
+
+    sources_content = selector.css("div.z--mb-16 a.tec--badge::text").getall()
+    sources = []
+    for item in sources_content:
+        sources.append(item.strip())
+
+    categories_content = selector.css("a.tec--badge--primary::text").getall()
+    categories = []
+    for item in categories_content:
+        categories.append(item.strip())
+
+    new_dict = {
+        "url": url,
+        "title": title_content,
+        "timestamp": timestamp,
+        "writer": writer,
+        "shares_count": int(shares),
+        "comments_count": int(coment),
+        "summary": summary_join,
+        "sources": sources,
+        "categories": categories
+    }
+
+    return(new_dict)
 
 
 # Requisito 3
 def scrape_novidades(html_content):
     """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    links = selector.css("h3.tec--card__title a::attr(href)").getall()
+    return links
 
 
 # Requisito 4
 def scrape_next_page_link(html_content):
     """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    loadMore = selector.css(".tec--btn--primary::attr(href)").get()
+    return loadMore
 
 
 # Requisito 5
 def get_tech_news(amount):
     """Seu código deve vir aqui"""
+    url_fetch = "https://www.tecmundo.com.br/novidades"
+    loops = (amount // 20.1) + 1
+    list_links = []
+    count_loops = 0
+    list_dict_news = []
+
+    while count_loops < loops:
+        html_content = fetch(url_fetch)
+        page_links_scraped = scrape_novidades(html_content)
+        url_fetch = scrape_next_page_link(html_content)
+        count_loops += 1
+        for link in page_links_scraped:
+            if len(list_links) < amount:
+                list_links.append(link)
+
+    for link in list_links:
+        html_content = fetch(link)
+        dict_news = scrape_noticia(html_content)
+        list_dict_news.append(dict_news)
+
+    create_news(list_dict_news)
+
+    return list_dict_news
