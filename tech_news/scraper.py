599bfd7b64672236a58cb66c48b34386eee3233e
nicole.teisen <nicole.camargo@gmail.com>
2021-09-07 16:12:12 -0300

    requisito 1

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,15 @@
+import requests
+import time
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    time.sleep(1)
+    try:
+        response = requests.get(url, timeout=3)
+        response.raise_for_status()
+        return response.text
+    except Exception:
+        return None
 
 
 # Requisito 2
