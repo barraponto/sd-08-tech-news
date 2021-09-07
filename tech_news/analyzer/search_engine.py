2c5067d83bf0ec681acf54c31c3b40ecf14753aa
Anderson Santana <anderslash@gmail.com>
2021-09-07 20:40:57 -0300

    feat: search engine

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,18 +1,43 @@
+import re
+from tech_news.database import search_news
+from datetime import datetime
+
+
+"""Ref: estudante Paulo Simões"""
+
+
+def serialize(news_list):
+    return [(news['title'], news['url']) for news in news_list]
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    """From a given string, return news with matching title"""
+    news = search_news({'title': re.compile(title, re.IGNORECASE)})
+    return serialize(news)
 
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    """From a given date, return matching news"""
+    try:
+        datetime.fromisoformat(date).timestamp()
+    except ValueError:
+        raise ValueError('Data inválida')
+
+    news = search_news({'timestamp': re.compile(date)})
+    return serialize(news)
 
 
 # Requisito 8
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    """From a given string, return news witch matching source"""
+    news = search_news({'sources': re.compile(source, re.IGNORECASE)})
+    return serialize(news)
 
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    """From a given string, return news with matching categories"""
+    news = search_news({'categories': re.compile(category, re.IGNORECASE)})
+    return serialize(news)
