9668d7cbc9fc88c50c0f503d5869243ff8572306
Ana Karine <anakarine.ti@gmail.com>
2021-09-06 21:30:40 -0300

    def search_by_date

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,8 @@
 import tech_news.database as db
+from datetime import datetime
+
+# Referências:
+# https://stackoverflow.com/questions/18039680/django-get-only-date-from-datetime-strptime
 
 
 # Requisito 6
@@ -18,6 +22,22 @@ def search_by_title(title):
 # Requisito 7
 def search_by_date(date):
     """Seu código deve vir aqui"""
+    news = db.find_news()
+    result = []
+
+    try:
+        for info in news:
+            if datetime.strptime(
+                info['timestamp'], "%Y-%m-%dT%H:%M:%S"
+            ).date() == datetime.strptime(
+                date, "%Y-%m-%d"
+            ).date():
+                result.append(
+                    (info["title"], info["url"])
+                )
+            return result
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
