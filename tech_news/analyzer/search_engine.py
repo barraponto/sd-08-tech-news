dd6b9bc806259dc7acf60a219af6f618fb8dca6a
Nicole Teisen Camargo <nicole.camargo@gmail.com>
2021-09-07 23:00:01 -0300

    requisito 7,8,9

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+from datetime import datetime
 
 # Requisito 6
 
@@ -14,14 +15,38 @@ def search_by_title(title):
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    format_date = "%Y-%m-%d"
+
+    try:
+        datetime.strptime(date, format_date)
+        news = []
+        get_news_by_date = search_news(
+            {"timestamp": {"$regex": date}}
+        )
+        for news_info in get_news_by_date:
+            news.append(tuple([news_info["title"], news_info["url"]]))
+        return news
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    news = []
+    get_news_by_source = search_news(
+        {"sources": {"$regex": source, "$options": "i"}}
+    )
+    for news_info in get_news_by_source:
+        news.append(tuple([news_info["title"], news_info["url"]]))
+    return news
 
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    news = []
+    get_news_by_category = search_news(
+        {"categories": {"$regex": category, "$options": "i"}}
+    )
+    for news_info in get_news_by_category:
+        news.append(tuple([news_info["title"], news_info["url"]]))
+    return news
