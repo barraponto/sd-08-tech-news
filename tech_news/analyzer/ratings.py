0b83a10851278782d9af854ea6422aa900c37d40
Nicole Teisen Camargo <nicole.camargo@gmail.com>
2021-09-07 23:05:57 -0300

    requisito 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,25 @@
+from tech_news.database import find_news
+
 # Requisito 10
+
+
 def top_5_news():
-    """Seu código deve vir aqui"""
+    top_five = []
+    get_news = find_news()
+    for item in get_news:
+        item["popularity"] = item["comments_count"] + item["shares_count"]
+
+    get_news = sorted(get_news, key=lambda x: x["title"])
+    get_news = sorted(get_news, key=lambda x: x["popularity"], reverse=True)
+
+    top_five = [
+        get_news for get_news in get_news[:5]
+    ]
+
+    for item in top_five:
+        top_five.append(tuple([item["title"], item["url"]]))
+
+    return top_five
 
 
 # Requisito 11
