8e81dbb71eb75bb96b1a85029053e66a77a95bd8
Gustavo Thirion - Turma 8 <gustavo.fthirion@gmail.com>
2021-09-08 17:36:14 -0300

    requisito 9

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -17,7 +17,10 @@ def search_by_title(title):
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    """Possível buscar uma notícia pela data com sucesso; Ao buscar por uma
+    data que não existe, o retorno deve ser uma lista vazia; Ao buscar por uma
+    data com formato inválido, deve lançar um erro ValueError com a mensagem
+    Data inválida."""
 
     # https://qastack.com.br/programming/16870663/how-do-i-validate-a-date-string-format-in-python
     try:
@@ -34,7 +37,9 @@ def search_by_date(date):
 
 # Requisito 8
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    """Possível buscar uma notícia pela fonte com sucesso; Ao buscar por uma
+    fonte que não existe, o retorno deve ser uma lista vazia; Possível buscar
+    uma notícia tanto pela fonte em maiúsculas como em minúsculas;"""
 
     myquery = {
         "sources": {"$elemMatch": {"$regex": source, "$options": "i"}}
@@ -46,4 +51,14 @@ def search_by_source(source):
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    """Possível buscar uma notícia pela categoria com sucesso; Ao buscar por
+    uma categoria que não existe, o retorno deve ser uma lista vazia; Possível
+    buscar uma notícia tanto pela categoria em maiúsculas como em
+    minúsculas;"""
+
+    myquery = {
+        "categories": {"$elemMatch": {"$regex": category, "$options": "i"}}
+    }
+    search_results = search_news(myquery)
+
+    return [(result["title"], result["url"]) for result in search_results]
