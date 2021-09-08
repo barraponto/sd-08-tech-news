a5bd47777cee31b87d49652976a1d7255b8e2aaf
Rita Jeveaux <ritajeveaux@gmail.com>
2021-09-08 15:07:40 -0300

    Req10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -4,14 +4,28 @@ from tech_news.database import find_news
 # Requisito 10
 def top_5_news():
     news = find_news()
+    if len(news) == 0:
+        return news
     top_news = []
     for new in news:
-        top_news = top_news.append(
-            new["shares_count"] + news["comments_count"],
-            news['title'], news['url']
-        )
-        top_five = sorted(top_news)
-    return top_five[:5]
+        top_news.append((
+            new["shares_count"] + new["comments_count"],
+            new['title'], new['url']
+        ))
+    news_sorted = sorted(top_news, key=lambda x: x[0], reverse=True)
+    if len(news_sorted) < 5:
+        n = len(news_sorted)
+    else:
+        n = 5
+    top_five = []
+    m = 0
+    while n > 0:
+        top_five.append((news_sorted[m][1], news_sorted[m][2]))
+        n -= 1
+        m += 1
+        print(n)
+    print(top_five)
+    return top_five
 
 
 # Requisito 11
