ef0f1f836a0f30b49c799930889421a7752f2edb
Igor <igorln96@gmail.com>
2021-09-07 22:11:15 -0300

    req 1 e 2

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,11 +1,49 @@
+import requests
+import time
+from parsel import Selector
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    time.sleep(1)
+
+    try:
+        response = requests.get(url, timeout=3)
+
+        if response.status_code == 200:
+            return response.text
+        else:
+            return None
+    except requests.ReadTimeout:
+        return None
 
 
 # Requisito 2
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    return {
+        "url": selector.css("head link[rel=canonical]::attr(href)").get(),
+        "title": selector.css(".tec--article__header__title::text").get(),
+        "timestamp": selector.css("#js-article-date::attr(datetime)").get(),
+        "writer": selector.css(".tec--author__info__link::text").get().strip(),
+        "shares_count": int(
+            selector.css(".tec--toolbar__item::text").get().split(" ")[1]
+        ),
+        "comments_count": int(
+            selector.css("#js-comments-btn::attr(data-count)").get()
+        ),
+        "summary": "".join(
+            selector.css(
+                ".tec--article__body p:first-of-type *::text"
+            ).getall()
+        ),
+        "sources": [
+            i.strip()
+            for i in selector.css("[class='tec--badge']::text").getall()
+        ],
+        "categories": [
+            i.strip() for i in selector.css("#js-categories a::text").getall()
+        ],
+    }
 
 
 # Requisito 3
