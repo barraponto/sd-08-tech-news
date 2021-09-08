fa49efa23aaa33475b68b6adc63f4a9342d3f248
victor Farias <victorfarias.tv@gmail.com>
2021-09-08 14:16:09 -0300

    Requisito 11

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,6 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
     """Seu código deve vir aqui"""
@@ -5,4 +8,12 @@ def top_5_news():
 
 # Requisito 11
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    news_list = find_news()
+    categories_result_list = []
+    if len(news_list) > 0:
+        for news in news_list:
+            categories_result_list.extend(news["categories"])
+    else:
+        return []
+    categories_result_list.sort()
+    return categories_result_list[:5]
