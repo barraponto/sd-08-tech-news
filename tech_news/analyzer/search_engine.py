0eb82cf614b00abc89ca48657c2afbe904d7e93a
Gustavo Thirion - Turma 8 <gustavo.fthirion@gmail.com>
2021-09-08 14:42:13 -0300

    requisito 7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,9 +1,12 @@
 from tech_news.database import search_news
+from datetime import datetime
 
 
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    """Possível buscar uma notícia pelo título com sucesso; Ao buscar por um
+    título que não existe, o retorno é uma lista vazia; Possível buscar uma
+    notícia com sucesso, tanto pelo título em maiúsculas como em minúsculas."""
 
     # https://www.w3schools.com/python/python_mongodb_query.asp
     myquery = {"title": {"$regex": title, "$options": 'i'}}
@@ -15,6 +18,18 @@ def search_by_title(title):
 # Requisito 7
 def search_by_date(date):
     """Seu código deve vir aqui"""
+
+    # https://qastack.com.br/programming/16870663/how-do-i-validate-a-date-string-format-in-python
+    try:
+        datetime.strptime(date, '%Y-%m-%d')
+    except ValueError:
+        raise ValueError("Data inválida")
+
+    # https://www.w3schools.com/python/python_mongodb_query.asp
+    myquery = {"timestamp": {"$regex": date}}
+
+    search_results = search_news(myquery)
+    return [(result["title"], result["url"]) for result in search_results]
 
 
 # Requisito 8
