636f1ac1b6942b1ea0836966096725ef6e87bc8e
Rosiele David <rosieleserpa@hotmail.com>
2021-09-09 17:43:37 -0300

    Requisito 4 concluído

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -36,6 +36,8 @@ def scrape_novidades(html_content):
 # Requisito 4
 def scrape_next_page_link(html_content):
     """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    return selector.css(".tec--list .tec--btn::attr(href)").get()
 
 
 # Requisito 5
