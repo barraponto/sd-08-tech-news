6a39a589bc3b317916da50074d0a1903fbcfc6f4
amandaos <amanda.775.souza@gmail.com>
2021-09-09 15:18:02 -0300

    Inicia projeto e implementa as funcoes fetch e scrape_noticia

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,11 +1,62 @@
+import requests
+import time
+from parsel import Selector
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    try:
+        response = requests.get(url, timeout=3)
+        time.sleep(1)
+        if response.status_code != 200:
+            return None
+        else:
+            return response.text
+    except requests.ReadTimeout:
+        return None
 
 
 # Requisito 2
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    link = selector.css("link[rel=canonical]::attr(href)").get()
+    title = selector.css(".tec--article__header__title::text").get()
+    timestamp = selector.css("time::attr(datetime)").get()
+    author = selector.css(".z--font-bold").css("*::text").get() or ""
+    shares_count = (
+        selector.css("div.tec--toolbar__item::text").get() or "0"
+    )
+    comments_count = (
+        selector.css("#js-comments-btn ::attr(data-count)").get() or "0"
+    )
+    summary = "".join(
+        selector.css(
+            "div .tec--article__body > p:nth-child(1) ::text"
+        ).getall()
+    )
+    sources = [
+        source.strip() for source in selector.css(
+            "div .z--mb-16 > div > a"
+        ).xpath("text()").getall()
+    ]
+    categories = [
+        category.strip() for category in selector.css(
+            "#js-categories > a"
+        ).xpath("text()").getall()
+    ]
+
+    result = {
+        "url": link,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": author.strip(),
+        "shares_count": int(shares_count.strip().split()[0]),
+        "comments_count": int(comments_count),
+        "summary": summary,
+        "sources": sources,
+        "categories": categories,
+    }
+    return result
 
 
 # Requisito 3
