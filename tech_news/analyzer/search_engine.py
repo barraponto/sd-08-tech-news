d964bb6b209095b804759b418c6407488fe36f20
Rosiele David <rosieleserpa@hotmail.com>
2021-09-09 19:29:22 -0300

    Requisito 6 concluído

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,20 @@
+from tech_news.database import db
+
+
 # Requisito 6
 def search_by_title(title):
     """Seu código deve vir aqui"""
+    search_title = list(
+        db.news.find(
+            {"title": {"$regex": title, "$options": "i"}},
+            {"_id": 0, "title": 1, "url": 1},
+        )
+    )
+
+    if len(search_title) == 0:
+        return search_title
+
+    return [(search_title[0]["title"], search_title[0]["url"])]
 
 
 # Requisito 7
