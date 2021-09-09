51cf780962ae24300bdefd720444bc67f5ab2a2f
Rosiele David <rosieleserpa@hotmail.com>
2021-09-09 20:27:25 -0300

    Requisito 10 concluído

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,27 @@
+from tech_news.database import db
+
+
 # Requisito 10
 def top_5_news():
     """Seu código deve vir aqui"""
+    top_5 = list(
+        db.news.aggregate(
+            [
+                {
+                    "$addFields": {
+                        "popularidade": {
+                            "$sum": ["$shares_count", "$comments_count"]
+                        },
+                    },
+                },
+                {"$sort": {"popularidade": -1}},
+                {"$limit": 5},
+                {"$project": {"_id": 0, "url": 1, "title": 1}},
+            ],
+        ),
+    )
+
+    return [(top["title"], top["url"]) for top in top_5]
 
 
 # Requisito 11
