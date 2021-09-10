0149d98733c2ff3873741e605fee560f1b073a81
Cleberton Francisco <clebertonfgc@gmail.com>
2021-09-09 22:00:10 -0300

    Requisito 8 e 9

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -31,9 +31,21 @@ def search_by_date(date):
 
 # Requisito 8
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    query_1 = db.search_news({"sources": {"$regex": source, "$options": "i"}})
+    results = []
+
+    for value in query_1:
+        results.append(((value["title"], value["url"])))
+
+    return results
 
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    query_1 = db.search_news({"categories": {"$regex": category, "$options": "i"}})
+    results = []
+
+    for value in query_1:
+        results.append(((value["title"], value["url"])))
+
+    return results
