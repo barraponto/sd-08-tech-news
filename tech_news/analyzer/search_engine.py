daa4ec4907f0ae9bd928d13cb19cb9f54d415395
Wesley Paulo Ribeiro <wesley_paulo00@hotmail.com>
2021-09-09 22:26:36 -0300

    R06 - Concluido

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,15 @@
+import tech_news.database as db
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    news = db.find_news()
+    news_list = []
+    for item in news:
+        if item["title"].lower() == title.lower():
+            content = (item["title"], item["url"])
+            news_list.append(content)
+    return news_list
 
 
 # Requisito 7
