acf0931fedab1d6cc3277e1e37a412982ed12c5f
gabrielDonnantuoni <gabrieldonnantuoni@gmail.com>
2021-09-10 12:07:15 -0300

    feat: Add tech_news.scraper.fetch implementation

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,24 @@
+from ratelimit import limits, sleep_and_retry
+import requests
+
+
 # Requisito 1
+@sleep_and_retry
+@limits(calls=1, period=1)
 def fetch(url):
-    """Seu código deve vir aqui"""
+    """
+    Make an HTTP request with GET Method to the url received as param.
+
+    Returns the HTML text if receive an response with status code is 200 within
+    3 seconds, otherwise returns None.
+    """
+    try:
+        response = requests.get(url, timeout=3)
+        if response.status_code != 200:
+            return None
+        return response.text
+    except requests.Timeout:
+        return None
 
 
 # Requisito 2
