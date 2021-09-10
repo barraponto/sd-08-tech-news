3026197f36785e55ef070347337feb00bb7289e3
Rodrigo Leite <rods.leite27@gmail.com>
2021-09-10 02:44:07 -0300

    req 7, 8

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,10 +1,15 @@
 from tech_news.database import search_news
 from datetime import datetime
 
+
+def array_filter(array):
+    return [(array["title"], array["url"]) for array in array]
+
+
 # Requisito 6
 def search_by_title(title):
-    news = search_news({"title": {"$regex": title, "$options": "i"} })
-    return [(news["title"], news["url"]) for news in news ]
+    news = search_news({"title": {"$regex": title, "$options": "i"}})
+    return array_filter(news)
 
 
 # Requisito 7
@@ -13,15 +18,17 @@ def search_by_date(date):
         datetime.fromisoformat(date)
     except ValueError:
         raise ValueError("Data inválida")
-    news = search_news({"timestamp": {"$regex": date, "$options": "i"} })
-    return [(news["title"], news["url"]) for news in news ]
+    news = search_news({"timestamp": {"$regex": date, "$options": "i"}})
+    return array_filter(news)
 
 
 # Requisito 8
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    news = search_news({"sources": {"$regex": source, "$options": "i"}})
+    return array_filter(news)
 
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    news = search_news({"categories": {"$regex": category, "$options": "i"}})
+    return array_filter(news)
