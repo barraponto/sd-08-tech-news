5c300dc76b852ef9a5af40520f9723ba2e258f8b
vitor <vitornunes200@gmail.com>
2021-09-10 11:33:11 -0300

    Req 10 e 11

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,8 +1,42 @@
+from tech_news.database import find_news
+import operator
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    
+    news = []
+    result = find_news()
+    
+    for index in result:
+        index["popularity"] = index["shares_count"] + index["comments_count"]
+
+    # result.sort(key=operator.itemgetter("sum"), reverse=True)
+    sorted_result = sorted(
+        result, key=lambda news: news["popularity"],
+        reverse=True
+    )
+
+    for index in sorted_result:
+        news.append((index["title"], index["url"]))
+
+    news_return = news[:5]
+    return news_return
+
 
 
 # Requisito 11
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    
+    categories = []
+    result = find_news()
+    
+    if len(result) > 0:
+        for index in result:
+            categories.extend(index["categories"])
+    else:
+        return []
+
+    categories.sort()
+    categories_return = categories[:5]
+    
+    return categories_return
