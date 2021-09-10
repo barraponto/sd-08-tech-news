3eeaec580da2eb1a00220280a3852f1ade9591cc
Fernanda Marques <nandateixmarques@gmail.com>
2021-09-09 22:02:28 -0300

    add requisito 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,27 @@
+from tech_news.database import db
+
+
 # Requisito 10
 def top_5_news():
     """Seu código deve vir aqui"""
+    list_results = list(
+        db.news.aggregate(
+            [
+                {
+                    "$addFields": {
+                        "popularidade": {
+                            "$sum": ["$shares_count", "$comments_count"]
+                        },
+                    },
+                },
+                {"$sort": {"popularidade": -1}},
+                {"$limit": 5},
+                {"$project": {"_id": 0, "url": 1, "title": 1}},
+            ],
+        ),
+    )
+
+    return [(news["title"], news["url"]) for news in list_results]
 
 
 # Requisito 11
